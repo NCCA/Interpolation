2c29cb10ea920ebc397b30929d8860d20d23707c
jmacey <jmacey@bournemouth.ac.uk>
2017-09-19 18:27:49 +0100

    updated for new ShaderLib structure all calls to set uniforms now use OverLoaded setUniform method

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -88,7 +88,7 @@ void NGLScene::initializeGL()
   // set the shape using FOV 45 Aspect Ratio based on Width and Height
   // The final two are near and far clipping planes of 0.5 and 10
   m_cam.setShape(45,(float)width()/height(),0.05,350);
-  shader->setShaderParam3f("viewerPos",m_cam.getEye().m_x,m_cam.getEye().m_y,m_cam.getEye().m_z);
+  shader->setUniform("viewerPos",m_cam.getEye().toVec3());
   // now create our light this is done after the camera so we can pass the
   // transpose of the projection matrix to the light to do correct eye space
   // transformations
@@ -119,10 +119,10 @@ void NGLScene::loadMatricesToShader()
   MVP=  MV*m_cam.getProjectionMatrix();
   normalMatrix=MV;
   normalMatrix.inverse();
-  shader->setShaderParamFromMat4("MV",MV);
-  shader->setShaderParamFromMat4("MVP",MVP);
-  shader->setShaderParamFromMat3("normalMatrix",normalMatrix);
-  shader->setShaderParamFromMat4("M",M);
+  shader->setUniform("MV",MV);
+  shader->setUniform("MVP",MVP);
+  shader->setUniform("normalMatrix",normalMatrix);
+  shader->setUniform("M",M);
 }
 
 void NGLScene::paintGL()
@@ -299,14 +299,14 @@ void NGLScene::keyPressEvent(QKeyEvent *_event)
     update();
 }
 
-void NGLScene::timerEvent( QTimerEvent *_event)
+void NGLScene::timerEvent( QTimerEvent *)
 {
   if(m_animate==true)
   {
-    m_time+=0.01;
-    if (m_time >=1.0)
+    m_time+=0.01f;
+    if (m_time >=1.0f)
     {
-      m_time=0.0;
+      m_time=0.0f;
     }
     update();
   }
